bf1163dadff8c823a3a53cc8575a3023f91faf4e
jbcanillo <jbcanillo@gmail.com>
2024-09-25 12:58:01 -0400

    final

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,7 +7,20 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json=myobj, headers=header)
     formatted_response = json.loads(response.text)
-    result = formatted_response['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(result, key=result.get)
-    result['dominant_emotion'] = dominant_emotion
+
+    # Add error handling
+    if response.status_code == 200:
+        result = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(result, key=result.get)
+        result['dominant_emotion'] = dominant_emotion
+    elif response.status_code == 400 or response.status_code == 500:
+        result = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
     return result
--- server.py
+++ server.py
@@ -1,30 +1,47 @@
+"""
+Emotion Detector Flask Application.
+
+This module provides a web application for detecting emotions 
+from text using the Emotion Detection service. It includes 
+routes for analyzing text and rendering the index page.
+"""
+
 from flask import Flask, render_template, request
-from EmotionDetection.emotion_detection import emotion_detector 
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    """Analyze the text for emotions and return the results."""
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Pass the text to the emotion_detector function and store the response
     response = emotion_detector(text_to_analyze)
 
+    # Show invalid text when result is none
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!."
+
     # Initialize an empty list to collect emotion strings
     emotions = []
+
     # Extract the values from the response and convert everything to string
     for key, value in response.items():
         if key != "dominant_emotion":
             emotions.append(f"'{key}': {value}")
+
     # Join the emotions list into a single string
     emotions_str = ", ".join(emotions)
 
     # Return a formatted string
-    return "For the given statement, the system response is {}. The dominant emotion is {}.".format(emotions_str, response['dominant_emotion'])
+    return f"For the given statement, the system response is { emotions_str }. \
+    The dominant emotion is { response['dominant_emotion']}."
 
 @app.route("/")
 def render_index_page():
+    """Render the index page."""
     return render_template('index.html')
 
 if __name__ == "__main__":
